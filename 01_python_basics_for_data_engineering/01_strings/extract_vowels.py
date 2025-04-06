9a136f2fd4cc2315b96569a6ebd606d30df0461c
Shamas Imran <shamas.imran@gmail.com>
2025-04-06 14:47:29 +0500

    Update extract_vowels.py
    ran and tested the code on local environment

--- 01_python_basics_for_data_engineering/01_strings/extract_vowels.py
+++ 01_python_basics_for_data_engineering/01_strings/extract_vowels.py
@@ -1,6 +1,15 @@
 def extract_vowels(text):
     vowels = "aeiouAEIOU"
     return [char for char in text if char in vowels]
+# This is a list comprehension that extracts vowels from the input text.
+
+"""
+result = []
+for char in text:
+    if char in vowels:
+        result.append(char)
+"""
+
 
 if __name__ == "__main__":
     print(extract_vowels("Data Engineering with Python"))
