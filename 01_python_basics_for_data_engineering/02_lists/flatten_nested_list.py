dd354481d353967c763b096ccb81ed48c4367da7
Shamas Imran <shamas.imran@gmail.com>
2025-04-19 15:03:49 +0500

    added comments

--- 01_python_basics_for_data_engineering/02_lists/flatten_nested_list.py
+++ 01_python_basics_for_data_engineering/02_lists/flatten_nested_list.py
@@ -1,5 +1,10 @@
 def flatten(nested_list):
     return [item for sublist in nested_list for item in sublist]
+    # This line flattens the nested list by iterating through each sublist in the nested list,
+    # and then iterating through each item in those sublists to create a single flat list.
+
 
 if __name__ == "__main__":
-    print(flatten([[1, 2], [3, 4], [5]]))
+    nestedList = [[1, 2], [3, 4], [5]]
+    output = flatten(nestedList)  
+    print(output)
--- 01_python_basics_for_data_engineering/03_dicts/frequency_counter.py
+++ 01_python_basics_for_data_engineering/03_dicts/frequency_counter.py
@@ -1,5 +1,6 @@
 def word_frequency(text):
     words = text.lower().split()
+            # This line converts the input text to lowercase and splits it into a list of words.
     return {word: words.count(word) for word in set(words)}
 
 if __name__ == "__main__":
